7a9013cf9633ae6789991f52c982ae4652211eff
Lukas Vanek <lukas@sinesquare.com>
2017-11-29 13:37:17 +0100

    build 2017-11-29

---
+++ packages/tabbdashboard/rand.js
@@ -0,0 +1,12 @@
+const items = [
+  'Pracovni duvody',
+  'Pracovní vytížení',
+  'Pracovni schuzka',
+  'Lékař',
+  'Rodinné důvody',
+  'KOnference NATO',
+  'Nemocen',
+  'návštěva lékaře'
+]
+
+console.log(items[Math.floor(Math.random()*items.length)]);
