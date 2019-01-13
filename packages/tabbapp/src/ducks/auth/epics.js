0d30f40f21bf1a2868a4a8eefc96fe22cf468de7
Lukas Vanek <lukas@sinesquare.com>
2019-01-13 07:11:26 +0100

    loginError Epic

--- packages/tabbapp/src/ducks/auth/epics.js
+++ packages/tabbapp/src/ducks/auth/epics.js
@@ -1,136 +1,63 @@
 // @flow
 import { Observable } from 'rxjs';
 import * as API from 'tabb-front/dist/api/user';
-import types from './types';
-import { loginFail, loginDone, logoutDone, signupFail } from './actions';
 import { checkinInit } from 'ducks/restaurant/actions';
 import { loadUserData } from 'ducks/app/actions';
 import { setCookie } from 'tabb-front/dist/lib/FN';
-import moment from 'moment';
+import { actionTypes } from 'react-redux-firebase'
 
-const loginInitEpic = (action$: Observable) =>
+
+const loginErrorEpic = (action$: Observable, { getFirebase }) =>
   action$
-    .ofType(types.LOGIN_INIT)
-    .switchMap(({ payload: { email, password, qr } }) => {
-      return Observable.fromPromise(API.Login({ email, password }))
-        .mergeMap(res => {
-          setCookie('access_token', res.token, 30);
-          // window.history.back();
-          return Observable.of(loginDone(res), loadUserData(), checkinInit({ qr }));
-        })
-        .catch(error => Observable.of(loginFail(error)))
-    });
+    .ofType(actionTypes.LOGIN_ERROR)
+    .mergeMap(({ authError }) => {
+      if (!authError) return Observable.of({ type: 'LOGIN_ERROR_HANDLED' });
+      if (authError.code === 'auth/account-exists-with-different-credential') {
 
-const fbAuthInitEpic = (action$: Observable) =>
-  action$
-    .ofType(types.FBAUTH_INIT)
-    .switchMap(({ payload: { fbRes, qr } }) => {
-      const { name, email, accessToken, gender, birthday } = fbRes;
-      return Observable.fromPromise(API.LoginWithFacebook({ accessToken }))
-        .mergeMap(res => {
-          console.log(res, 'LoginWithFacebook');
-          if (res.token) setCookie('access_token', res.token, 30);
-          window.history.back();
-          return Observable.of(loginDone(res), loadUserData(), checkinInit({ qr }));
-        })
-        .catch(error => {
-          console.log(error,'LoginWithFacebookError');
 
-          let formatedGender = 'OTHER';
-          if (gender === 'male') formatedGender = 'MALE';
-          if (gender === 'female') formatedGender = 'FEMALE';
-          let formatedBirthday = null;
-          if (birthday) formatedBirthday = moment(birthday, "MM/DD/YYYY").format("YYYY-MM-DD")
 
-          return Observable.fromPromise(API.Register({
-              name,
-              phone: String(Math.floor(Math.random() * 9283899800) + 22838998),
-              email,
-              accessToken,
-              registrationType: 'FACEBOOK',
-              birthday: formatedBirthday,
-              gender: formatedGender
-            }))
-            .mergeMap(res => {
-              console.log(res, 'Register');
-              if (res.metadata) setCookie('access_token', res.metadata.token, 30);
-              window.history.back();
-              return Observable.of(loginDone(res), loadUserData(), checkinInit({ qr }));
-            })
-            .catch(error => {
-              console.log(error, 'RegisterError');
-              return Observable.of(loginFail(error))
-            })
-          // return Observable.of(loginFail(error))
+        getFirebase().auth().fetchSignInMethodsForEmail(authError.email).then((methods) => {
+          console.log('Attempting providers', methods);
+
+          // TODO
+          // if (method === 'password') {
+          // signInWithEmailAndPassword
+          //   return this.loginUser(error.email, params.password).then(result => {
+          //     console.log('Reauthentication result', result);
+          //     result.user.linkAndRetrieveDataWithCredential(pendingCred).then((usercred) => {
+          //       console.log('New credential successfully linked', usercred);
+          //     }).catch(error => {
+          //       console.log('Credential linking error', error);
+          //     });
+          //   });
+          // }
+          // console.log('Reattempting login with social', method);
+          //
+          // return firebase.auth().signInWithPopup(this.getProvider(method)).then(result => {
+          //   console.log('Reauthentication result', result);
+          //   result.user.linkAndRetrieveDataWithCredential(pendingCred).then((usercred) => {
+          //     console.log('New credential successfully linked', usercred);
+          //   }).catch(error => {
+          //     console.log('Credential linking error', error);
+          //   });
+          // }).catch(error => {
+          //   console.log('Reauthentication error', error);
+          // });
+
+
         });
+
+
+
+        console.log(authError);
+      }
+      return Observable.of({ type: 'LOGIN_ERROR_HANDLED' });
     });
 
-const googleAuthInitEpic = (action$: Observable) =>
-  action$
-    .ofType(types.GOOGLE_AUTH_INIT)
-    .switchMap(({ payload: { googleRes, qr } }) => {
-      const { profileObj, accessToken } = googleRes;
-      console.log(googleRes);
-      const { name, email } = profileObj;
-      return Observable.fromPromise(API.LoginWithGoogle({ accessToken }))
-        .mergeMap(res => {
-          console.log(res, 'LoginWithGoogle');
-          if (res.token) setCookie('access_token', res.token, 30);
-          window.history.back();
-          return Observable.of(loginDone(res), loadUserData(), checkinInit({ qr }));
-        })
-        .catch(error => {
-          console.log(error,'LoginWithGoogleError');
 
-          return Observable.fromPromise(API.Register({
-              name,
-              phone: String(Math.floor(Math.random() * 9283899800) + 22838998),
-              email,
-              accessToken,
-              registrationType: 'GOOGLE',
-            }))
-            .mergeMap(res => {
-              console.log(res, 'Register');
-              if (res.metadata) setCookie('access_token', res.metadata.token, 30);
-              window.history.back();
-              return Observable.of(loginDone(res), loadUserData(), checkinInit({ qr }));
-            })
-            .catch(error => {
-              console.log(error, 'RegisterError');
-              return Observable.of(loginFail(error))
-            })
-          // return Observable.of(loginFail(error))
-        });
-    });
-
-const signupInitEpic = (action$: Observable) =>
-  action$
-    .ofType(types.SIGNUP_INIT)
-    .switchMap(({ payload: { name, phone, email, password, qr } }) =>
-      Observable.fromPromise(API.Register({
-          name, phone, email, password, registrationType: 'LOCAL'
-        }))
-        .mergeMap(res => {
-          if (res.metadata) setCookie('access_token', res.metadata.token, 30);
-          return Observable.of(loginDone(res), loadUserData(), checkinInit({ qr }));
-        })
-        .catch(error => Observable.of(signupFail(error))),
-    );
-
-const logoutInitEpic = (action$: Observable) =>
-  action$
-    .ofType(types.LOGOUT_INIT)
-    .mergeMap(() => {
-      setCookie('access_token', '', 30);
-      return Observable.of(logoutDone(), loadUserData());
-    });
 
 
 
 export default [
-  loginInitEpic,
-  fbAuthInitEpic,
-  googleAuthInitEpic,
-  signupInitEpic,
-  logoutInitEpic,
+  loginErrorEpic,
 ];
