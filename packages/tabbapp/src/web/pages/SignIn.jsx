0850a5afe45327a831ca6c05626abd8a1d4426dd
Lukas Vanek <lukas@sinesquare.com>
2019-01-16 04:59:17 +0100

    Use case: Email entered, assoc w/ 3rd party only

--- packages/tabbapp/src/web/pages/SignIn.jsx
+++ packages/tabbapp/src/web/pages/SignIn.jsx
@@ -85,7 +85,8 @@ class SignInForm extends React.Component {
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
       errors.email = 'Invalid email address';
     }
-    if (Object.keys(errors).length !== 0) throw new SubmissionError(errors);
+    // TODO correct regexp
+    // if (Object.keys(errors).length !== 0) throw new SubmissionError(errors);
   }
 
   signIn = ({ email, password }) => {
@@ -127,14 +128,15 @@ class SignInForm extends React.Component {
       }
       // present user with dialog with options
       else { // TODO
-        auth.prompt('account-exists', {
-          attempt: 'password',
-          method: methods[0],
-          email,
-          action: () => {
-            return auth.loginSocial(methods[0]);
-          }
-        });
+        firebase.login({ provider: methods[0].replace('.com',''), type: 'popup' });
+        // auth.prompt('account-exists', {
+        //   attempt: 'password',
+        //   method: methods[0],
+        //   email,
+        //   action: () => {
+        //     return auth.loginSocial(methods[0]);
+        //   }
+        // });
       }
     });
   }
