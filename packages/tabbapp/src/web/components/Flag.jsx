1079241d7d4766c17d6363860c19b125403132e8
Máté Homolya <mate.homolya@gmail.com>
2019-01-18 23:52:16 -0700

    Flag styling adjustment

--- packages/tabbapp/src/web/components/Flag.jsx
+++ packages/tabbapp/src/web/components/Flag.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import flags from 'lib/flags.json';
-import flagIcons from 'icons/flags';
+import flags from 'icons/flags';
 
 const styles = theme => ({
   root: {
-    width: 25.2,
-    height: 18,
     borderRadius: 3,
     overflow: 'hidden',
     border: `1px solid ${theme.palette.divider}`,
-  },
-  rootAlt: {
-    width: 24,
-    height: 18,
+    marginLeft: 1,
   }
 })
 
@@ -22,7 +16,7 @@ const Flag = ({
   country = 'us',
 }) => {
   return (
-    <img alt={country} className={classes.root} src={flagIcons[country]} />
+    <img alt={country} className={classes.root} src={flags[country]} />
   );
 };
 
