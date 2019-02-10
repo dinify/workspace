5a1588df91a2d7d76e491bb9d331c781947248ec
Lukas Vanek <lukas@sinesquare.com>
2019-02-10 19:12:54 +0100

    cors

--- packages/dinifyadmin/server.js
+++ packages/dinifyadmin/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 import Restaurants from './models/Restaurants';
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import bodyParser from 'body-parser';
 import mongoconnect from './mongoconnect';
@@ -9,6 +10,7 @@ import { Translate } from '@google-cloud/translate';
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
