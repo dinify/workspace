be8f6ddca7f2e325f57cfa3dbd95127f726fa7e2
Máté Homolya <mate.homolya@gmail.com>
2019-02-15 16:25:59 +1100

    Finalize language picker dialog

--- packages/common/src/components/dialogs/LanguagePickerDialog.jsx
+++ packages/common/src/components/dialogs/LanguagePickerDialog.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import R from 'ramda';
 import { compose } from 'redux';
-import { withState } from 'recompose';
+import { withStateHandlers } from 'recompose';
+import { Motion, spring } from 'react-motion';
 import { withStyles } from '@material-ui/core/styles';
+import { parseLanguages } from '../../lib/FN';
+import match from 'autosuggest-highlight/umd/match';
+import parse from 'autosuggest-highlight/umd/parse';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import TextField from '@material-ui/core/TextField';
@@ -12,13 +17,20 @@ import DialogContent from '@material-ui/core/DialogContent';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ChevronRight from '@material-ui/icons/ChevronRightRounded';
+import ChevronLeft from '@material-ui/icons/ChevronLeftRounded';
 
 import Flag from '../Flag';
-import languages from '../../lib/languages';
+import languagesRaw from '../../lib/languages';
 import countries from '../../lib/countries';
 
-const styles = theme => ({
+const languages = parseLanguages(languagesRaw);
 
+const styles = theme => ({
+  scrollingList: {
+    overflowY: 'scroll',
+    paddingTop: 8,
+    paddingBottom: 8
+  }
 })
 
 const LanguagePickerDialog = (props) => {
@@ -28,41 +40,159 @@ const LanguagePickerDialog = (props) => {
     setFilter,
     selectedLang,
     setSelectedLang,
+    page,
+    prevPage,
+    setPage,
     open = false,
+    onClose,
+    resetState,
+    initialSelectedLanguage,
     ...other
   } = props;
-  return (
-    <Dialog open={open} {...other}>
-      <DialogTitle>
-        Select language
-      </DialogTitle>
+
+  const filtered = R.filter(lang => {
+    if (!filter) return true;
+    return match(lang.name, filter).length + match(lang.nameNative, filter).length > 0;
+  }, languages);
+
+  const languageClickHandler = lang => {
+    const solo = lang.countries.length === 1;
+    if (solo) setSelectedLang(lang.countries[0].langtag);
+    else setPage(lang);
+  };
+
+  const countryClickHandler = country => {
+    setSelectedLang(country.langtag);
+  }
+
+  const highlightBold = text => {
+    if (!filter) return text;
+    return <span>
+      {parse(text, match(text, filter)).map((part, i) => {
+        return part.highlight ? (<b key={i}>{part.text}</b>) : part.text;
+      })}
+    </span>;
+  }
+
+  const animConfig = { stiffness: 480, damping: 48 };
+
+  const mainPage = (
+    <div style={{
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column'
+    }}>
       <div style={{padding: '0px 24px'}}>
         <TextField
           fullWidth
-          dense
           name="Search"
           label="Search"
           hint="Search"
           variant="filled"
-          value={filter}
-          onChange={evt => {setFilter(evt.target.text)}}/>
+          value={filter || ''}
+          onChange={event => setFilter(event.target.value)}/>
       </div>
       <Divider style={{marginTop: 16}}/>
-      <div style={{maxHeight: 240, overflowY: 'scroll', paddingTop: 8, paddingBottom: 8}}>
-        {languages.map((lang, i) => {
-          const solo = lang[3].length === 1;
+      <div className={classes.scrollingList} style={{flex: 1}}>
+        {filtered.map((lang, i) => {
+          const solo = lang.countries.length === 1;
+
+          let secondary = highlightBold(lang.nameNative);
+          if (lang.name.toLowerCase() === lang.nameNative.toLowerCase()) {
+            if (solo) secondary = (<i>{lang.countries[0].nameNative}</i>);
+            else secondary = '';
+          }
+
+          let selected = false;
+          if (selectedLang) selected = selectedLang.split('-')[0] === lang.code;
           return (
-            <ListItem key={i} dense button style={{paddingLeft: 24, paddingRight: 24}}
-              onClick={() => { /* TODO: display available regions for language */ }}>
-              <ListItemText primary={lang[1]} secondary={lang[2]}/>
-              {solo ? <Flag country={lang[3][0][0].split('-')[1]}/> : <ChevronRight style={{opacity: 0.54}}/>}
+            <ListItem
+              key={i} dense button selected={selected}
+              style={{paddingLeft: 24, paddingRight: 24}}
+              onClick={() => languageClickHandler(lang)}>
+              <ListItemText primary={highlightBold(lang.name)} secondary={secondary}/>
+              {solo ? <Flag country={lang.countries[0].regionCode}/> : <ChevronRight style={{opacity: 0.54}}/>}
             </ListItem>
           );
         })}
       </div>
+    </div>
+  );
+  let dialogContent;
+  let currentPage = page || prevPage;
+  if (currentPage) {
+    dialogContent = (<div style={{
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column'
+    }}>
+      <div>
+        <Button style={{margin: '0px 24px'}} onClick={() => setPage(null)}>
+          <ChevronLeft />
+          Back
+        </Button>
+      </div>
+      <Divider style={{marginTop: 16}}/>
+      <div className={classes.scrollingList} style={{flex: 1}}>
+        {currentPage.countries.map((country, i) => {
+          const name = countries[country.regionCode];
+          return (
+            <ListItem key={i} dense button
+              selected={country.langtag === selectedLang}
+              style={{paddingLeft: 24, paddingRight: 24}}
+              onClick={() => countryClickHandler(country)}>
+              <ListItemText primary={name} secondary={country.nameNative}/>
+              <Flag country={country.regionCode}/>
+            </ListItem>
+          );
+        })}
+      </div>
+    </div>);
+  }
+
+  return (
+    <Dialog open={open} onClose={() => {
+      resetState();
+      onClose(null);
+    }} {...other}>
+      <DialogTitle>
+        {page ? 'Select country' : 'Select language'}
+      </DialogTitle>
+      <div style={{
+        height: 312,
+        width: 240,
+        overflow: 'hidden'
+      }}>
+        <Motion
+          defaultStyle={{x: 0}}
+          style={{x: spring(page ? 1 : 0, animConfig)}}>
+          {style => {
+            return (
+              <div style={{
+                height: '100%',
+                display: 'flex',
+                transform: `translate3d(${-240 * style.x}px, 0, 0)`
+              }}>
+                <div style={{minWidth: 240}}>
+                  {mainPage}
+                </div>
+                <div style={{minWidth: 240}}>
+                  {dialogContent}
+                </div>
+              </div>
+            );
+          }}
+        </Motion>
+      </div>
       <Divider />
       <DialogActions>
-        <Button  color="primary" onClick={() => {/* TODO: return selected language */}}>
+        <Button
+          color="primary"
+          disabled={!selectedLang}
+          onClick={() => {
+            resetState();
+            onClose(selectedLang)
+          }}>
           Select
         </Button>
       </DialogActions>
@@ -71,8 +201,31 @@ const LanguagePickerDialog = (props) => {
 };
 
 const enhance = compose(
-  withState('filter', 'setFilter'),
-  withState('selectedLang', 'setSelectedLang'),
+  withStateHandlers(
+    ({ initialSelectedLanguage }) => ({
+      selectedLang: initialSelectedLanguage,
+      page: null,
+      prevPage: null,
+      filter: ''
+    }),
+    {
+      setFilter: () => (value) => ({
+        filter: value,
+      }),
+      setPage: () => (value) => {
+        if (value == null) return { page: value };
+        else return { page: value, prevPage: value };
+      },
+      setSelectedLang: () => (value) => ({
+        selectedLang: value,
+      }),
+      resetState: () => (value) => ({
+        selectedLang: null,
+        page: null,
+        filter: ''
+      })
+    }
+  ),
   withStyles(styles)
 );
 
