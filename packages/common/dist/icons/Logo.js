08ea55d577496404c28686b0ddfb0e68e4bf79a3
Máté Homolya <mate.homolya@gmail.com>
2019-02-21 12:03:15 +1100

    Updated logo icon

--- packages/common/dist/icons/Logo.js
+++ packages/common/dist/icons/Logo.js
@@ -17,8 +17,7 @@ var _default = function _default(props) {
   return _react.default.createElement(_SvgIcon.default, _extends({
     viewBox: "0 0 24 24"
   }, props), _react.default.createElement("path", {
-    id: "Logo",
-    d: "M22,6.7L22,20L2,20l0-9.9l5.8,4.5l1.1,0.9l1.2-0.8L22,6.7 M24,3L9,13L0,6l0,16l24,0L24,3L24,3z"
+    d: "M12,22.4l-1-1C10.1,20.5,8.9,20,7.5,20H1.1L0,4h6.6c1.8,0,3.5,0.5,5,1.5L12,5.8l0.4-0.3c1.5-1,3.2-1.5,5-1.5 H24l-1.1,16h-6.4c-1.3,0-2.6,0.5-3.5,1.5L12,22.4z M2.9,18h4.6c1.7,0,3.2,0.6,4.5,1.6c1.3-1.1,2.8-1.6,4.5-1.6h4.6l0.8-12h-4.4 c-1.4,0-2.7,0.4-3.9,1.2l-1.5,1l-1.5-1C9.3,6.4,8,6,6.6,6H2.1L2.9,18z"
   }));
 };
 
