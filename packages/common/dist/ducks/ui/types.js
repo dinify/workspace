7f32ad6fe9834b212f9a03e5e9bfcf2efe04a720
Máté Homolya <mate.homolya@gmail.com>
2019-04-11 23:04:27 +1000

    Fix auth dialogs and autofill color

--- packages/common/dist/ducks/ui/types.js
+++ packages/common/dist/ducks/ui/types.js
@@ -5,8 +5,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 var _default = {
-  DIALOG_OPEN: 'tabb/ui/DIALOG_OPEN',
-  DIALOG_CLOSE: 'tabb/ui/DIALOG_CLOSE',
-  TOGGLE_THEME: 'tabb/ui/TOGGLE_THEME'
+  DIALOG_OPEN: 'dinify/ui/DIALOG_OPEN',
+  DIALOG_CLOSE: 'dinify/ui/DIALOG_CLOSE',
+  TOGGLE_THEME: 'dinify/ui/TOGGLE_THEME'
 };
 exports.default = _default;
