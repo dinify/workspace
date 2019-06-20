7d444be51c77b5caf2cdc34aef2d4af903b6eb3d
Lukas Vanek <lukas@sinesquare.com>
2019-06-20 15:00:48 +0200

    Functions: selectTaTargets fix

--- packages/functions/src/http/selectTaTargets.f.ts
+++ packages/functions/src/http/selectTaTargets.f.ts
@@ -15,14 +15,15 @@ const cors = require('cors')({
 
 exports = module.exports = functions.region('europe-west1').https.onRequest((req, res) => {
   cors(req, res, () => {
+    const defaultFilter = {
+      campaignStatuses: [], // ['landed:landing', 'authorized'] campaign_statuses
+      emailStatuses: [], // ['dispatched', 'clicked'] events_sg
+      targetingTagLabels: [] // ['seelction-1'] targeting_taggables, targeting_tags
+    };
     const {
       segment = 'default', // segment-1
       campaign = 'default', // rp-onboarding
-      filter = {
-        campaignStatuses: [], // ['landed:landing', 'authorized'] campaign_statuses
-        emailStatuses: [], // ['dispatched', 'clicked'] events_sg
-        targetingTagLabels: [] // ['seelction-1'] targeting_taggables, targeting_tags
-      }
+      filter = defaultFilter,
     } = req.body;
 
     if (!filter) {
@@ -34,6 +35,10 @@ exports = module.exports = functions.region('europe-west1').https.onRequest((req
     const extKey = 'location_id';
     const extType = 'App\\Models\\RestaurantTa';
 
+    const filterTargetingTagLabels = filter.targetingTagLabels || [];
+    const filterCampaignStatuses = filter.campaignStatuses || [];
+    const filterEmailStatuses = filter.emailStatuses || [];
+
     sequelize.query(`
       select *
       from ${extTable}
@@ -42,14 +47,14 @@ exports = module.exports = functions.region('europe-west1').https.onRequest((req
       	from ${extTable} as ext
       	join targeting_taggables as ttg on ext.${extKey}=ttg.item_id
       	join targeting_tags as tta on ttg.targeting_tag_id=tta.id
-      	where label in ('${filter.targetingTagLabels.join("','")}')
+      	where label in ('${filterTargetingTagLabels.join("','")}')
       )
       or ${extKey} in (
         select ${extKey}
         from ${extTable} as ext
         join targets on ext.${extKey}=targets.target_id
         join campaign_statuses as cps on targets.id=cps.target_id
-        where cps.status in ('${filter.campaignStatuses.join("','")}')
+        where cps.status in ('${filterCampaignStatuses.join("','")}')
       )
       or ${extKey} in (
         select ${extKey}
@@ -57,7 +62,7 @@ exports = module.exports = functions.region('europe-west1').https.onRequest((req
         join targets on ext.${extKey}=targets.target_id
         join emails on targets.id=emails.target_id
         join events_sg as sg on emails.sg_message_id=sg.sg_message_id
-        where sg.event in ('${filter.emailStatuses.join("','")}')
+        where sg.event in ('${filterEmailStatuses.join("','")}')
       )
     `).then(([results, metadata]) => {
       if (!results.length) {
