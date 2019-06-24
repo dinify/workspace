2d343e240ea44babbeacb62d9db8069055ab8c62
Lukas Vanek <lukas@sinesquare.com>
2019-06-24 17:32:07 +0200

    Functions: emailWebhook fix & refactor

--- packages/functions/src/http/emailWebhook.f.ts
+++ packages/functions/src/http/emailWebhook.f.ts
@@ -1,34 +1,62 @@
 import * as functions from "firebase-functions";
 import Emails from "../models/Emails";
 import EventsSg from "../models/EventsSg";
+import each from 'async/each';
 
 const cors = require('cors')({
   origin: true,
 });
 
+const handleError = (errorMessage, cb, error?) => {
+  console.error(errorMessage);
+  if (error) console.error(error);
+  cb(errorMessage);
+}
+
 exports = module.exports = functions.region('europe-west1').https.onRequest((req, res) => {
   cors(req, res, () => {
     const events = req.body || [];
 
-    events.map((eventObject) => {
-      const { email, event, timestamp, sg_message_id } = eventObject;
+    console.log(events);
 
-      Emails.findOne({
-        where: {
-          message_id: sg_message_id,
-          message_key: 'sg_message_id'
+    each(
+      events,
+      (eventObject: any, cb) => {
+        const { sg_message_id } = eventObject;=
+        Emails.findOne({
+          where: {
+            message_id: sg_message_id,
+            message_key: 'sg_message_id'
+          }
+        }).then((emailResult: any) => {
+          if (emailResult) {
+            EventsSg.create({
+              email_id: emailResult.id,
+              email: eventObject.email,
+              timestamp: eventObject.timestamp,
+              smtp_id: eventObject['smtp-id'],
+              event: eventObject.event,
+              category: eventObject.category,
+              sg_event_id: eventObject.sg_event_id,
+              sg_message_id: eventObject.sg_message_id
+            }).then(() => {
+              cb(null);
+            }).catch((error) =>
+              handleError('EventsSg creation error', cb, error)
+            );
+          } else {
+            handleError('no email found for this event', cb);
+          }
+        }).catch((error) =>
+          handleError('no email found for provided sg_message_id', cb, error)
+        );
+      }, (error) => {
+        if (!error) {
+          res.json({ error: null});
+        } else {
+          res.json({ error });
         }
-      }).then((emailResult: any) => {
-        if (emailResult) {
-          EventsSg.create({
-            email_id: emailResult.id,
-            ...eventObject
-          }).then((o) => {
-            res.sendStatus(200);
-          }).catch((error) => res.json({ error }));
-        }
-
-      }).catch((error) => res.json({ error }));
-    })
+      }
+    )
   });
 });
