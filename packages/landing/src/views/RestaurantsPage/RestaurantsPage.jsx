38e95b09532f9549a25f5b614b387860e2372816
Máté Homolya <mate.homolya@gmail.com>
2019-07-13 22:05:23 +0200

    Landing / RP: add image in hero, make it larger

--- packages/landing/src/views/RestaurantsPage/RestaurantsPage.jsx
+++ packages/landing/src/views/RestaurantsPage/RestaurantsPage.jsx
@@ -43,41 +43,51 @@ class RestaurantsPage extends React.Component {
 
     return (
       <div>
-        <Header
-          color="transparent"
-        />
-        <div className={classNames(classes.section, classes.container)}>
-          <GridContainer>
-            <GridItem style={{color: 'white'}}>
-              <Typography variant="h4" color="inherit">
-                {t('restaurantsPage.hero.title')}
-              </Typography>
-              <Typography variant="h6" style={{color: 'rgba(255, 255, 255, 0.76)', marginBottom: 16}}>
-                {t('restaurantsPage.hero.subtitle')}
-              </Typography>
-
-              <Button
-                variant="contained"
-                color="primary"
-                style={{ height: 48, marginTop: 24 }}
-                href={registrationURL}
-                target="_blank"
-                rel="noopener noreferrer"
-                id="getStartedButton"
-              >
-                <OpenInNew style={{ marginRight: 8 }}/>
-                {t('restaurantsPage.hero.cta')}
-              </Button>
-              <Typography variant="caption" style={{marginTop: 8, color: 'rgba(255, 255, 255, 0.76)'}}>
-                Start your free 30 day trial
-              </Typography>
-              {email && (
-                <Typography variant="body2" style={{color: 'rgba(255, 255, 255, 0.76)', marginTop: 8}}>
-                  {t('restaurantsPage.hero.continueWith', {email})}
+        <div style={{overflow: 'hidden', position: 'relative'}}>
+          <Header
+            color="transparent"
+          />
+          <div className={classNames(classes.section, classes.container)}>
+            <GridContainer>
+              <GridItem style={{color: 'white', marginTop: 64, marginBottom: 64}}>
+                <Typography variant="h4" color="inherit">
+                  {t('restaurantsPage.hero.title')}
                 </Typography>
-              )}
-            </GridItem>
-          </GridContainer>
+                <Typography variant="h6" style={{color: 'rgba(255, 255, 255, 0.76)', marginBottom: 16}}>
+                  {t('restaurantsPage.hero.subtitle')}
+                </Typography>
+
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{ height: 48, marginTop: 24 }}
+                  href={registrationURL}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  id="getStartedButton"
+                >
+                  <OpenInNew style={{ marginRight: 8 }}/>
+                  {t('restaurantsPage.hero.cta')}
+                </Button>
+                <Typography variant="caption" style={{marginTop: 8, color: 'rgba(255, 255, 255, 0.76)'}}>
+                  Start your free 30 day trial
+                </Typography>
+                {email && (
+                  <Typography variant="body2" style={{color: 'rgba(255, 255, 255, 0.76)', marginTop: 8}}>
+                    {t('restaurantsPage.hero.continueWith', {email})}
+                  </Typography>
+                )}
+              </GridItem>
+            </GridContainer>
+          </div>
+          <div
+            className={classNames(classes.heroImg, classes.darkScrim)}
+            alt="Restaurant atmoshpere"
+            style={{
+              zIndex: -1,
+              backgroundImage: `url("https://storage.googleapis.com/static.dinify.app/landing/rp-landing.jpg")`
+            }}
+          />
         </div>
         <div className={classes.lightBg}>
           <SectionWaiterboard t={t}/>
--- packages/landing/src/views/RestaurantsPage/restaurantsPageStyle.jsx
+++ packages/landing/src/views/RestaurantsPage/restaurantsPageStyle.jsx
@@ -14,6 +14,31 @@ const restaurantsPageStyle = theme => ({
   },
   lightBg: {
     backgroundColor: theme.palette.background.paper
+  },
+  heroImg: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+    backgroundRepeat: "no-repeat"
+  },
+  darkScrim: {
+    "&:before": {
+      backgroundColor: theme.palette.action.active
+    },
+    "&:after,&:before": {
+      position: "absolute",
+      zIndex: "1",
+      width: "100%",
+      height: "100%",
+      display: "block",
+      left: "0",
+      top: "0",
+      content: "''"
+    }
   }
 });
 
