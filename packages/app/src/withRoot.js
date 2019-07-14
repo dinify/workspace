891e71164d1ffcd34b9027fbb7b00a6b05ef8521
Máté Homolya <mate.homolya@gmail.com>
2019-07-14 23:00:06 +0200

    Common: Remove json translations (non NMT)

--- packages/app/src/withRoot.js
+++ packages/app/src/withRoot.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import MuiPickersUtilsProvider from 'material-ui-pickers/utils/MuiPickersUtilsProvider';
 import DateFnsUtils from 'material-ui-pickers/utils/date-fns-utils';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import blueGrey from '@material-ui/core/colors/blueGrey';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import getTheme from "@dinify/common/dist/theme";
 
@@ -21,73 +20,6 @@ primary_700    #CE3F40
 primary_800    #C13939
 primary_900    #B1312F
 */
-
-const getThemeOld = dark => createMuiTheme(
-  {
-    palette: {
-      type: dark ? 'dark' : 'light',
-      primary: {
-        light: '#E14846', // primary_600
-        main: '#C13939', // primary_800
-        dark: '#B1312F', // primary_900
-      },
-      secondary: {
-        light: blueGrey[300],
-        main: blueGrey[500],
-        dark: blueGrey[700],
-      },
-      text: {
-        primary: dark ? 'rgba(255, 255, 255, 0.87)' : 'rgba(0, 0, 0, 0.72)',
-        secondary: dark ? 'rgba(255, 255, 255, 0.54)' : 'rgba(0, 0, 0, 0.38)',
-      },
-    },
-    typography: {
-      useNextVariants: true,
-      // System font stack
-      /* fontFamily: [
-        'Lato',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-      ].join(','), */
-    },
-    overrides: {
-      MuiInputBase: {
-        root: {
-          [`& > input:-webkit-autofill,
-            & > input:-webkit-autofill:hover,
-            & > input:-webkit-autofill:focus`]: {
-            WebkitTextFillColor: dark ? 'rgba(255, 255, 255, 0.87)' : 'rgba(0, 0, 0, 0.72)',
-            WebkitBoxShadow: `0 0 0px 1000px rgba(0,0,0,0) inset !important`,
-            WebkitTransitionDelay: '99999s',
-            caretColor: dark ? 'rgba(255, 255, 255, 0.87)' : 'rgba(0, 0, 0, 0.72)',
-          }
-        }
-      },
-      MuiFilledInput: {
-        root: {
-          borderRadius: 4,
-          overflow: 'hidden'
-        },
-        underline: {
-          '&:before': {
-            borderBottom: 'none',
-          },
-          '&:hover:not($disabled):not($focused):not($error):before': {
-            borderBottom: 'none',
-          },
-        }
-      }
-    }
-  },
-);
 
 function withRoot(Component) {
   const lightTheme = getTheme({ type: 'light' });
