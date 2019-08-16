a0a4e7b524954b19048397a442a0bf20d89c1d7c
Lukas Vanek <lukas@sinesquare.com>
2019-08-16 14:50:47 +0200

    Waiterboard: ducks/order refactor

--- packages/waiterboard/src/ducks/order/epics.js
+++ packages/waiterboard/src/ducks/order/epics.js
@@ -1,32 +1,36 @@
 import { from } from 'rxjs';
 import { mergeMap, switchMap, catchError } from 'rxjs/operators';
 import { ofType } from 'redux-observable';
-import pluck from 'ramda/src/pluck'
+import pluck from 'ramda/src/pluck';
 import * as API from '@dinify/common/dist/api/restaurant';
 import { handleEpicAPIError } from '@dinify/common/dist/lib/FN';
+import { fetchAllUsers } from 'ducks/user/actions';
+import * as orderTypes from 'ducks/order/types';
 
 const loadOrderEpic = (action$, $state) =>
   action$.pipe(
-    ofType('LOAD_ORDER_INIT'),
+    ofType(orderTypes.LOAD_ORDER_INIT),
     switchMap((action) => {
+
       const waiterboardId = $state.value.restaurant.selectedWBId;
+
       return from(API.GetOrders({ waiterboardId })).pipe(
         mergeMap((orders) => {
-          const userIds = pluck('initiator', orders).filter((id) => id.length === 24)
+
+          const userIds = pluck('initiator', orders);
+
           return [
+            fetchAllUsers({ ids: userIds, cache: true }),
             {
-              type: 'FETCHALL_USER_INIT',
-              payload: { ids: userIds, cache: true, node: true }
-            },
-            {
-              type: 'LOAD_ORDER_DONE',
-              payload: {res: orders }
+              type: orderTypes.LOAD_ORDER_DONE,
+              payload: { res: orders }
             }
-          ]
+          ];
+          
         }),
         catchError(error => handleEpicAPIError({
           error,
-          failActionType: 'ERROR',
+          failActionType: orderTypes.LOAD_ORDER_FAIL,
           initAction: action
         }))
       )
@@ -35,4 +39,4 @@ const loadOrderEpic = (action$, $state) =>
 
 export default [
   loadOrderEpic
-]
+];
--- packages/waiterboard/src/ducks/order/types.js
+++ packages/waiterboard/src/ducks/order/types.js
@@ -1,3 +1,6 @@
 const p = 'dinify/order';
 
 export const ORDER_RECEIVED = `${p}/ORDER_RECEIVED`;
+export const LOAD_ORDER_INIT = `${p}/LOAD_ORDER_INIT`;
+export const LOAD_ORDER_DONE = `${p}/LOAD_ORDER_DONE`;
+export const LOAD_ORDER_FAIL = `${p}/LOAD_ORDER_FAIL`;
