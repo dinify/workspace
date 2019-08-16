69e901d5d520fdf9579f5db03bfd8f9c4908a6fd
Lukas Vanek <lukas@sinesquare.com>
2019-08-16 16:38:21 +0200

    Waiterboard: ducks/table refactor

--- packages/waiterboard/src/ducks/table/epics.js
+++ packages/waiterboard/src/ducks/table/epics.js
@@ -2,42 +2,49 @@ import { of, from } from 'rxjs';
 import { switchMap, map, catchError, filter } from 'rxjs/operators';
 import * as API from '@dinify/common/dist/api/restaurant';
 import { handleEpicAPIError } from '@dinify/common/dist/lib/FN';
+import * as tableTypes from 'ducks/table/types';
 
 const clearTableEpic = (action$) =>
   action$.pipe(
-    filter(action => action.type === 'CLEAR_TABLE_INIT'),
+    filter(action => action.type === tableTypes.CLEAR_TABLE_INIT),
     switchMap((action) => {
       const { payload } = action;
+
       if (!global.confirm('Do you really want to check-out this table?')) {
-        return of({type: "CLEAR_TABLE_CANCELED"});
+        return of({type: tableTypes.CLEAR_TABLE_CANCELED});
       }
+
       return from(API.CheckOut({ tableId: payload.table.id, node: true })).pipe(
-        map(() => ({type: 'CLEAR_TABLE_DONE', payload })),
+        map(() => ({ type: tableTypes.CLEAR_TABLE_DONE, payload })),
         catchError(error => handleEpicAPIError({
           error,
-          failActionType: 'CLEAR_TABLE_FAIL',
+          failActionType: tableTypes.CLEAR_TABLE_FAIL,
           initAction: action
         }))
       )
+
     })
   );
 
 const clearUserEpic = (action$) =>
   action$.pipe(
-    filter(action => action.type === 'CLEAR_USER_INIT'),
+    filter(action => action.type === tableTypes.CLEAR_USER_INIT),
     switchMap((action) => {
       const { payload } = action;
+
       if (!global.confirm('Do you really want to check-out this user?')) {
-        return of({type: "CLEAR_USER_CANCELED"});
+        return of({ type: tableTypes.CLEAR_USER_CANCELED });
       }
+
       return from(API.CheckOutUser({userId: payload.userId, node: true })).pipe(
-        map(() => ({type: 'CLEAR_USER_DONE', payload })),
+        map(() => ({ type: tableTypes.CLEAR_USER_DONE, payload })),
         catchError(error => handleEpicAPIError({
           error,
-          failActionType: 'CLEAR_USER_FAIL',
+          failActionType: tableTypes.CLEAR_USER_FAIL,
           initAction: action
         }))
       )
+
     })
   );
 
--- packages/waiterboard/src/ducks/table/types.js
+++ packages/waiterboard/src/ducks/table/types.js
@@ -3,5 +3,11 @@ const p = 'dinify/table';
 export const CLEAR_TABLE_INIT = `${p}/CLEAR_TABLE_INIT`;
 export const CLEAR_TABLE_DONE = `${p}/CLEAR_TABLE_DONE`;
 export const CLEAR_TABLE_FAIL = `${p}/CLEAR_TABLE_FAIL`;
+export const CLEAR_TABLE_CANCELED = `${p}/CLEAR_TABLE_CANCELED`;
+
+export const CLEAR_USER_INIT = `${p}/CLEAR_USER_INIT`;
+export const CLEAR_USER_DONE = `${p}/CLEAR_USER_DONE`;
+export const CLEAR_USER_FAIL = `${p}/CLEAR_USER_FAIL`;
+export const CLEAR_USER_CANCELED = `${p}/CLEAR_USER_CANCELED`;
 
 export const UPDATE_TABLE_INIT = `${p}/UPDATE_TABLE_INIT`;
