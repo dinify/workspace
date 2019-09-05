8b9c63b80dd9cef76afbc677f8d972df2ef91bba
Máté Homolya <mate.homolya@gmail.com>
2019-09-05 20:10:24 +0200

    App / Cart: update cart view

--- packages/app/src/web/components/cart/cart.view.tsx
+++ packages/app/src/web/components/cart/cart.view.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import CartPage from '../../pages/Cart';
 import { AppBar, AppBarAction, AppBarTitle } from '../../components/app-bar';
-
-import { Cart } from 'CartModels';
+import CartItem from './cart.item';
+import { Cart, Price } from 'CartModels';
 
 const CartView: React.FC<{
     onClose?: () => void,
     cart: Cart
 }> = ({cart, onClose = () => {}, ...otherProps}) => {
+    const [editMode, setEditMode] = useState(false);
     const { t } = useTranslation();
+
     const cartItemCount = (cart && cart.items) ? cart.items.length : 0;
+    console.log(cart);
+    
     return (
         <div {...otherProps}>
             <AppBar>
-                <AppBarAction type="close" onClick={onClose}/>
+                {!editMode && <AppBarAction type="close" onClick={onClose}/>}
                 <AppBarTitle 
                     title={t('cart.title')} 
                     subtitle={t('cart.itemCount', { count: cartItemCount, context: cartItemCount === 0 ? 'none' : undefined })}/>
+                <AppBarAction type={editMode ? 'done' : 'edit'} onClick={() => {setEditMode(!editMode)}}/>
             </AppBar>
+            {cart.items.map(item => {
+                return (
+                    <CartItem editMode={editMode} cartItem={item}/>
+                );
+            })}
             <CartPage />
         </div>
     );
