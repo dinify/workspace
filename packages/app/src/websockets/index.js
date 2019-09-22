318b5852cd728f669bc010d8d7d9444f46b4def0
Lukas Vanek <lukas@sinesquare.com>
2019-09-22 18:01:30 +0200

    App: websockets index.js cleanup

--- packages/app/src/websockets/index.js
+++ packages/app/src/websockets/index.js
@@ -7,7 +7,6 @@ import { fetchBillAsync } from '../ducks/transaction/actions.ts';
 
 const socket = io('https://ws.dinify.app');
 
-
 const websockets = (store, i18nInstance) => {
   const { dispatch, getState } = store;
   const getUID = () => getState().firebase.auth.uid;
@@ -22,49 +21,61 @@ const websockets = (store, i18nInstance) => {
     initSocket();
   });
 
-  socket.on('transaction-status', (data) => {
-    const me = data.transaction.initiator === getUID();
-    if (data.transaction.status === 'PROCESSED') {
+  socket.on('transaction-status', (payload) => {
+    const me = payload.transaction.initiator === getUID();
+    if (payload.transaction.status === 'PROCESSED') {
       dispatch({
         type: getType(fetchBillAsync.request)
       });
       dispatch({
         type: types.CONFIRMED_PAYMENT,
-        payload: data
+        payload
       });
       if (me) {
-        dispatch(snackbar.show({ message: i18nInstance.t('paymentConfirmed') }));
+        dispatch(snackbar.show({
+          message: i18nInstance.t('paymentConfirmed')
+        }));
       }
     }
     else if (me) {
-      dispatch(snackbar.show({ message: i18nInstance.t('paymentCancelled') }));
+      dispatch(snackbar.show({
+        message: i18nInstance.t('paymentCancelled')
+      }));
     }
   });
 
-  socket.on('order-status', (data) => {
-    if (data.order.status === 'CONFIRMED') {
+  socket.on('order-status', (payload) => {
+    if (payload.order.status === 'CONFIRMED') {
       dispatch({
         type: getType(fetchBillAsync.request)
       });
       dispatch({
-        type: types.CONFIRMED_ORDER, payload: data
+        type: types.CONFIRMED_ORDER, payload
       });
-      if (data.order.initiator === getUID()) {
-        dispatch(snackbar.show({ message: i18nInstance.t('orderConfirmed') }));
+      if (payload.order.initiator === getUID()) {
+        dispatch(snackbar.show({
+          message: i18nInstance.t('orderConfirmed')
+        }));
       }
     }
-    else if (data.order.status === 'CANCELLED' && data.order.initiator === getUID()) {
-      dispatch(snackbar.show({ message: i18nInstance.t('orderCancelled') }));
+    else if (payload.order.status === 'CANCELLED' && payload.order.initiator === getUID()) {
+      dispatch(snackbar.show({
+        message: i18nInstance.t('orderCancelled')
+      }));
     }
   });
 
-  socket.on('call-status', (data) => {
-    if (data.call.status === 'CONFIRMED') {
-      dispatch({ type: types.CONFIRMED_CALL, payload: data });
-      dispatch(snackbar.show({ message: i18nInstance.t('serviceCallConfirmed') }));
+  socket.on('call-status', (payload) => {
+    if (payload.call.status === 'CONFIRMED') {
+      dispatch({ type: types.CONFIRMED_CALL, payload });
+      dispatch(snackbar.show({
+        message: i18nInstance.t('serviceCallConfirmed')
+      }));
     }
-    else if (data.call.status === 'CANCELLED') {
-      dispatch(snackbar.show({ message: i18nInstance.t('serviceCallCancelled') }));
+    else if (payload.call.status === 'CANCELLED') {
+      dispatch(snackbar.show({
+        message: i18nInstance.t('serviceCallCancelled')
+      }));
     }
   });
 
@@ -72,33 +83,43 @@ const websockets = (store, i18nInstance) => {
     const me = data.seat.user_id === getUID();
     dispatch({ type: types.CHECKIN, payload: { ...data, me } });
     if (!me) {
-      dispatch(snackbar.show({ message: i18nInstance.t('guestJoinedTable') }));
+      dispatch(snackbar.show({
+        message: i18nInstance.t('guestJoinedTable')
+      }));
     }
   });
 
-  socket.on('checkout', (data) => {
-    if (data.seat.userId === getUID()) {
-      dispatch({ type: types.CHECKOUT_ALL, payload: data });
-      dispatch(snackbar.show({ message: i18nInstance.t('checkedOut') }));
+  socket.on('checkout', (payload) => {
+    if (payload.seat.userId === getUID()) {
+      dispatch({ type: types.CHECKOUT_ALL, payload });
+      dispatch(snackbar.show({
+        message: i18nInstance.t('checkedOut')
+      }));
     }
     else {
-      dispatch({ type: types.CHECKOUT, payload: data });
-      dispatch(snackbar.show({ message: i18nInstance.t('guestLeftTable') }));
+      dispatch({ type: types.CHECKOUT, payload });
+      dispatch(snackbar.show({
+        message: i18nInstance.t('guestLeftTable')
+      }));
     }
   });
 
-  socket.on('checkout-all', (data) => {
-    dispatch({ type: types.CHECKOUT_ALL, payload: data });
-    dispatch(snackbar.show({ message: i18nInstance.t('checkedOut')}));
+  socket.on('checkout-all', (payload) => {
+    dispatch({ type: types.CHECKOUT_ALL, payload });
+    dispatch(snackbar.show({
+      message: i18nInstance.t('checkedOut')
+    }));
   });
 
-  socket.on('seats', (data) => {
-    dispatch({ type: types.SEATS, payload: data });
+  socket.on('seats', (payload) => {
+    dispatch({ type: types.SEATS, payload });
   });
 
-  socket.on('split', (data) => {
-    dispatch({ type: types.SPLIT, payload: data });
-    dispatch(snackbar.show({ message: i18nInstance.t('newBillSplitItems') }))
+  socket.on('split', (payload) => {
+    dispatch({ type: types.SPLIT, payload });
+    dispatch(snackbar.show({
+      message: i18nInstance.t('newBillSplitItems')
+    }))
   });
 
 }
