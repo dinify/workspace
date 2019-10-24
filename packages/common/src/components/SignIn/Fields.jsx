4dad4c964bfc1dc150c18a959d7199271b118ee6
Lukas Vanek <lukas@sinesquare.com>
2019-10-24 17:24:14 +0200

    Common: started fixing Fields.jsx

--- packages/common/src/components/SignIn/Fields.jsx
+++ packages/common/src/components/SignIn/Fields.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux'
-import { firebaseConnect } from 'react-redux-firebase';
+import { useFirebase } from 'react-redux-firebase'
 import { withStyles } from '@material-ui/core/styles';
 import { Field } from 'redux-form';
 import { Motion, spring } from 'react-motion';
@@ -50,14 +50,20 @@ const styles = theme => ({
 const UP = 1;
 const DOWN = -1;
 
-class Fields extends React.Component {
-  state = {
-    errors: {}
-  }
+const Fields = ({
+  t,
+  classes,
+  page,
+  showPassword,
+  setPage,
+  setShowPassword,
+  env,
+  authError
+}) => {
 
-  componentWillReceiveProps(nextProps) {
-    const { authError } = nextProps;
-    const { page, t } = this.props;
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
     let errors = {};
     if (!authError) return;
     switch (authError.code) {
@@ -82,235 +88,222 @@ class Fields extends React.Component {
       default:
         break;
     }
-
     // Clear form errors on page change
-    if (nextProps.page !== page) errors = {};
-    this.setState({ errors });
+    // if (nextProps.page !== page) errors = {};
+    setErrors({ errors });
+  });
+
+  const firebase = useFirebase();
+
+  const animConfig = { stiffness: 480, damping: 48 };
+
+  const formOpen = page !== 'default';
+
+  let emailLabel = t('email.long');
+
+  if (page === 'default') {
+    emailLabel = t('auth.continueWithEmail');
   }
 
-  render() {
-    const {
-      t,
-      classes,
-      firebase,
-      page,
-      showPassword,
-      setPage,
-      setShowPassword,
-      env
-    } = this.props;
-    const {
-      errors
-    } = this.state;
-    const animConfig = { stiffness: 480, damping: 48 };
+  const direction = env === 'DASHBOARD' ? DOWN : UP;
 
-    const formOpen = page !== 'default';
+  const Button = ({children, ...otherProps}) => 
+    <MuiButton classes={{root: classes.button2}} {...otherProps}>
+      {children}
+    </MuiButton>;
 
+  const emailField = (
+    <Field
+      name="email"
+      component={Text}
+      componentProps={{
+        label: emailLabel,
+        error: errors.email,
+        type: 'email',
+        fullWidth: true,
+        variant: 'filled',
+        name: 'email',
+        autocapitalization: 'none',
+        autoComplete: 'email',
+      }}
+    />
+  );
 
-    let emailLabel = t('email.long');
-
-    if (page === 'default') {
-      emailLabel = t('auth.continueWithEmail');
-    }
-
-    const direction = env === 'DASHBOARD' ? DOWN : UP;
-
-    const Button = ({children, ...otherProps}) => <MuiButton classes={{root: classes.button2}} {...otherProps}>{children}</MuiButton>;
-
-    const emailField = (
-      <Field
-        name="email"
-        component={Text}
-        componentProps={{
-          label: emailLabel,
-          error: errors.email,
-          type: 'email',
-          fullWidth: true,
-          variant: 'filled',
-          name: 'email',
-          autocapitalization: 'none',
-          autoComplete: 'email',
-        }}
-      />
-    );
-
-    const signupForm = style => (
+  const signupForm = style => (
+    <div className={classes && classes.background} style={{
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      zIndex: 50,
+      transform: `translate3d(0, ${style.x * 129 * direction}px, 0)`
+    }}>
       <div className={classes && classes.background} style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        zIndex: 50,
-        transform: `translate3d(0, ${style.x * 129 * direction}px, 0)`
+        position: 'relative',
+        zIndex: 70,
+        transform: `translate3d(0, ${style.x * 129 * (direction === DOWN ? 1 : 0)}px, 0)`
       }}>
-        <div className={classes && classes.background} style={{
-          position: 'relative',
-          zIndex: 70,
-          transform: `translate3d(0, ${style.x * 129 * (direction === DOWN ? 1 : 0)}px, 0)`
-        }}>
-          {emailField}
-        </div>
-        {['default', 'signUp'].includes(page) && <div style={{
-          position: 'relative',
-          display: 'flex',
-          marginTop: 8,
-          zIndex: 60,
-        }}>
-          <Field
-            name="firstName"
-            component={Text}
-            componentProps={{
-              label: t('auth.firstName'),
-              disabled: !formOpen,
-              type: 'text',
-              variant: 'filled',
-              name: 'fname',
-              autoFocus: true,
-              autoComplete: 'given-name',
-              autocapitalization: 'words',
-              style: {marginRight: 4, flex: 1}
-            }}
-          />
-          <Field
-            name="lastName"
-            component={Text}
-            componentProps={{
-              label: t('auth.lastName'),
-              disabled: !formOpen,
-              type: 'text',
-              variant: 'filled',
-              name: 'lname',
-              autoComplete: 'family-name',
-              autocapitalization: 'words',
-              style: {marginLeft: 4, flex: 1}
-            }}
-          />
-        </div>}
-        <Motion
-          defaultStyle={{x: 1}}
-          style={{x: spring(page === 'forgotPassword' ? 0 : 1, animConfig)}}>
-          {style =>
-            <div style={{position: 'relative', zIndex: 60}}>
-            <div ref={node => {this.socialSection = node}} style={{
-              willChange: 'transform',
-              overflow: 'hidden',
-              opacity: style.x**(1/3),
-              transformOrigin: 'top center',
-              transform: `scale(1, ${style.x}) translate3d(0, 0, 0)`
-            }}>
-              <div ref={node => {this.socialSection = node}} style={{
-                transformOrigin: 'top center',
-                transform: `scale(1, ${1 / style.x}) translate3d(0, 0, 0)`
-              }}>
-                <Field
-                  name="password"
-                  component={Text}
-                  meta={{error: errors.password}}
-                  componentProps={{
-                    label: t('auth.password'),
-                    style: {marginTop: 8},
-                    disabled: !formOpen,
-                    type: showPassword ? 'text' : 'password',
-                    fullWidth: true,
-                    variant: 'filled',
-                    name: 'password',
-                    autoComplete: page === 'signUp' ? 'new-password' : 'current-password',
-                    InputProps: {
-                      endAdornment: (
-                        <InputAdornment position="end">
-                          <IconButton
-                            disabled={!formOpen}
-                            aria-label="Toggle password visibility"
-                            onClick={() => {setShowPassword(!showPassword)}}>
-                            <ToggleIcon
-                              on={!showPassword}
-                              onIcon={<Visibility />}
-                              offIcon={<VisibilityOff />}/>
-                          </IconButton>
-                        </InputAdornment>
-                      ),
-                    }
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-          }
-        </Motion>
-        {['forgotPassword', 'signIn'].includes(page) &&
-          <Button
-            variant="outlined"
-            className={`${classes.uncapitalized} ${classes.transitionOpacity}`}
-            onClick={() => setPage('forgotPassword')}
-            style={{
-              position: 'relative',
-              zIndex: 60,
-              marginTop: 8,
-              opacity: page === 'forgotPassword' ? 0 : 1
-            }}>
-            Forgot password
-          </Button>
-        }
+        {emailField}
       </div>
-    );
-
-    const separator = (
-      <div style={{marginBottom: 16}} className={classes && classes.flex}>
-        <Divider className={classes && classes.grow} />
-        <div style={{height: 0, display: 'flex', alignItems: 'center'}}>
-          <Typography
-            variant="caption"
-            component="span"
-            color="textSecondary"
-            style={{ paddingLeft: 8, paddingRight: 8 }}>
-            {t('auth.or')}
-          </Typography>
-        </div>
-        <Divider className={classes && classes.grow} />
-      </div>
-    );
-
-    const mobile = getPlatform() !== 'desktop';
-    return (
+      {['default', 'signUp'].includes(page) && <div style={{
+        position: 'relative',
+        display: 'flex',
+        marginTop: 8,
+        zIndex: 60,
+      }}>
+        <Field
+          name="firstName"
+          component={Text}
+          componentProps={{
+            label: t('auth.firstName'),
+            disabled: !formOpen,
+            type: 'text',
+            variant: 'filled',
+            name: 'fname',
+            autoFocus: true,
+            autoComplete: 'given-name',
+            autocapitalization: 'words',
+            style: {marginRight: 4, flex: 1}
+          }}
+        />
+        <Field
+          name="lastName"
+          component={Text}
+          componentProps={{
+            label: t('auth.lastName'),
+            disabled: !formOpen,
+            type: 'text',
+            variant: 'filled',
+            name: 'lname',
+            autoComplete: 'family-name',
+            autocapitalization: 'words',
+            style: {marginLeft: 4, flex: 1}
+          }}
+        />
+      </div>}
       <Motion
         defaultStyle={{x: 1}}
-        style={{x: spring(formOpen ? 0 : 1, animConfig)}}>
-        {style => {
-          return (
-            <div style={{
-              position: 'relative'
+        style={{x: spring(page === 'forgotPassword' ? 0 : 1, animConfig)}}>
+        {style =>
+          <div style={{position: 'relative', zIndex: 60}}>
+          <div ref={node => {this.socialSection = node}} style={{
+            willChange: 'transform',
+            overflow: 'hidden',
+            opacity: style.x**(1/3),
+            transformOrigin: 'top center',
+            transform: `scale(1, ${style.x}) translate3d(0, 0, 0)`
+          }}>
+            <div ref={node => {this.socialSection = node}} style={{
+              transformOrigin: 'top center',
+              transform: `scale(1, ${1 / style.x}) translate3d(0, 0, 0)`
             }}>
-              {direction === DOWN && signupForm(style)}
-              <div style={{
-                position: 'absolute',
-                zIndex: 40,
-                top: direction === DOWN ? 72 : 0,
-                left: 0,
-                right: 0,
-                opacity: style.x,
-              }}>
-                {direction === DOWN && separator}
-                <div style={{ paddingBottom: 16 }}>
-                  <GoogleButton onClick={() => firebase.login({ provider: 'google', type: mobile ? 'redirect' : 'popup' })} />
-                </div>
-                <div style={{ paddingBottom: 16 }}>
-                  <FacebookButton onClick={() => firebase.login({ provider: 'facebook', type: mobile ? 'redirect' : 'popup' })} />
-                </div>
-                {direction === UP && separator}
-              </div>
-              {direction === UP && signupForm(style)}
+              <Field
+                name="password"
+                component={Text}
+                meta={{error: errors.password}}
+                componentProps={{
+                  label: t('auth.password'),
+                  style: {marginTop: 8},
+                  disabled: !formOpen,
+                  type: showPassword ? 'text' : 'password',
+                  fullWidth: true,
+                  variant: 'filled',
+                  name: 'password',
+                  autoComplete: page === 'signUp' ? 'new-password' : 'current-password',
+                  InputProps: {
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          disabled={!formOpen}
+                          aria-label="Toggle password visibility"
+                          onClick={() => {setShowPassword(!showPassword)}}>
+                          <ToggleIcon
+                            on={!showPassword}
+                            onIcon={<Visibility />}
+                            offIcon={<VisibilityOff />}/>
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }
+                }}
+              />
             </div>
-          );
-        }}
+          </div>
+        </div>
+        }
       </Motion>
-    );
-  }
+      {['forgotPassword', 'signIn'].includes(page) &&
+        <Button
+          variant="outlined"
+          className={`${classes.uncapitalized} ${classes.transitionOpacity}`}
+          onClick={() => setPage('forgotPassword')}
+          style={{
+            position: 'relative',
+            zIndex: 60,
+            marginTop: 8,
+            opacity: page === 'forgotPassword' ? 0 : 1
+          }}>
+          Forgot password
+        </Button>
+      }
+    </div>
+  );
+
+  const separator = (
+    <div style={{marginBottom: 16}} className={classes && classes.flex}>
+      <Divider className={classes && classes.grow} />
+      <div style={{height: 0, display: 'flex', alignItems: 'center'}}>
+        <Typography
+          variant="caption"
+          component="span"
+          color="textSecondary"
+          style={{ paddingLeft: 8, paddingRight: 8 }}>
+          {t('auth.or')}
+        </Typography>
+      </div>
+      <Divider className={classes && classes.grow} />
+    </div>
+  );
+
+  const mobile = getPlatform() !== 'desktop';
+  return (
+    <Motion
+      defaultStyle={{x: 1}}
+      style={{x: spring(formOpen ? 0 : 1, animConfig)}}>
+      {style => {
+        return (
+          <div style={{
+            position: 'relative'
+          }}>
+            {direction === DOWN && signupForm(style)}
+            <div style={{
+              position: 'absolute',
+              zIndex: 40,
+              top: direction === DOWN ? 72 : 0,
+              left: 0,
+              right: 0,
+              opacity: style.x,
+            }}>
+              {direction === DOWN && separator}
+              <div style={{ paddingBottom: 16 }}>
+                <GoogleButton onClick={() => firebase.login({ provider: 'google', type: mobile ? 'redirect' : 'popup' })} />
+              </div>
+              <div style={{ paddingBottom: 16 }}>
+                <FacebookButton onClick={() => firebase.login({ provider: 'facebook', type: mobile ? 'redirect' : 'popup' })} />
+              </div>
+              {direction === UP && separator}
+            </div>
+            {direction === UP && signupForm(style)}
+          </div>
+        );
+      }}
+    </Motion>
+  );
 }
 
 
 export default compose(
   withStyles(styles),
-  firebaseConnect(),
   connect(
     state => ({
       authError: state.firebase.authError,
