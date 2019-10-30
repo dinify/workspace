a108f900bbbcc924b7f063693fcc2ffe8914a764
Máté Homolya <mate.homolya@gmail.com>
2019-10-30 11:50:39 +0100

    App and Common: socket subcriber and provider

---
+++ packages/common/src/lib/socket/index.tsx
@@ -0,0 +1,21 @@
+import { createContext, useContext } from "react";
+
+export interface Socket extends SocketIOClient.Socket {
+  status?:
+    | "initialized"
+    | "connected"
+    | "disconnected"
+    | "failed"
+    | "connected"
+    | "reconnecting"
+    | "failed";
+}
+
+export const SocketContext = createContext<Socket | null>(null);
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+export * from "./provider";
+export * from "./subscription";
